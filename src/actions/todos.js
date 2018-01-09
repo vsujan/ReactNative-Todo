6b8d8ca80b4f77662844bad752a2a921d653efe8
Sujan Vaidya <shujan1992@gmail.com>
2018-01-09 14:23:30 +0545

    Dispatch action to fetch list of todos

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,28 +1,32 @@
 import * as types from './types';
+import todos from './todos.json';
 
 export function addNewTodo(todo) {
-  return (dispatch)
-  {
+  return dispatch => {
     dispatch(todoList(todo));
   }
 }
 
 export function updateTodo(todo) {
-  return (dispatch)
-  {
+  return dispatch => {
     dispatch(todoList(todo));
   }
 }
 
 export function deleteTodo(todo) {
-  return (dispatch)
-  {
+  return dispatch => {
     dispatch(todoList(todo))
   }
 }
 
+export function fetchTodos() {
+  return dispatch => {
+    dispatch(todoList(todos))
+  }
+}
 
-export const todoList = todos => {
+
+const todoList = (todos) => {
   return {
     type: types.TODO_LIST,
     data: todos
--- src/screens/todo/todo.js
+++ src/screens/todo/todo.js
@@ -6,9 +6,17 @@ import {
 
 export default class Todo extends Component<{}> {
 
+  componentDidMount() {
+    this.props.fetchTodoList();
+  }
+
+  componentWillReceiveProps(props) {
+    console.log('RECEIVED PROPS: ', props.todoList);
+  }
+
   render() {
     return (
-      <View>
+      <View style={{marginTop: 20}}>
         <Text>Todo list appears here.</Text>
       </View>
     )
